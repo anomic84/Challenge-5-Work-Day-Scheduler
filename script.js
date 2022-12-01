504df12e39247506fcc3864da88fb8ae61c2c399
anomic84@gmail.com <spencercarlson@Spencers-MacBook-Pro-2.local>
2022-12-01 14:04:39 -0800

    added Jquery function linking the time in the app to real time. Made a smart for loop. Set the class of col to continuously update with current time to show whether in past, present or future, by color. Added save button function to save what you enter in each hour section. Added it to JSON to save even when you refresh

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var saveBtn = $(".saveBtn")
+
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+    var currentDate = dayjs().format('MMM' + ',' + 'DD' + ',' + 'YYYY')
+    $("#currentDay").text(currentDate)
+
+    var currentTime = dayjs().hour()
+    var timeBlock = $(".time-block")
+    // Smart for loop
+    timeBlock.each(function () {
+        var hour = parseInt($(this).attr("id"))
+        // Set the class of col to present/past/future so colors visually reflect where user is at in the day
+        if (hour === currentTime) {
+            $(this).children(".col-md-10").attr("class", "present col-md-10 description")
+        } else if (hour < currentTime) {
+            $(this).children(".col-md-10").attr("class", "past col-md-10 description")
+        } else {
+            $(this).children(".col-md-10").attr("class", "future col-md-10 description")
+        }
+    })
+    // save button to local storage
+    saveBtn.on("click", function (event) {
+        event.preventDefault()
+        var value = $(this).siblings(".col-md-10").val().replace(key)
+        var key = $(this).parent().attr("id")
+        localStorage.setItem(key, JSON.stringify(value))
+    })
+    //jquery function to save text in each box to JSON 
+    $("#9 textarea").val(JSON.parse(localStorage.getItem("9")))
+    $("#10 textarea").val(JSON.parse(localStorage.getItem("10")))
+    $("#11 textarea").val(JSON.parse(localStorage.getItem("11")))
+    $("#12 textarea").val(JSON.parse(localStorage.getItem("12")))
+    $("#13 textarea").val(JSON.parse(localStorage.getItem("13")))
+    $("#14 textarea").val(JSON.parse(localStorage.getItem("14")))
+    $("#15 textarea").val(JSON.parse(localStorage.getItem("15")))
+    $("#16 textarea").val(JSON.parse(localStorage.getItem("16")))
+    $("#17 textarea").val(JSON.parse(localStorage.getItem("17")))
 });
